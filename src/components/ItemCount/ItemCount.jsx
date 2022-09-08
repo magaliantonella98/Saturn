4c574b6b78e065da483a87a170edf42db07e5e98
Magali Antonella <magaliantonella98@gmail.com>
2022-09-08 15:43:07 -0300

    Correcciones desafio p2

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+/*import React, {useState, useEffect} from 'react';
 
 const ItemCount = ({stock, initial, onAdd}) => {
     const [count, setCount] = useState(initial);
@@ -27,10 +27,10 @@ const ItemCount = ({stock, initial, onAdd}) => {
                 <button className="btn btn-outline-secondary" onClick={decrement}>Restar</button>
                 <p style={{margin:'1rem'}}>{count}</p>
                 <button className="btn btn-outline-secondary" onClick={increment}>Sumar</button>
-            <button className="btn btn-outline-secondary" onClick={onAdd}>Add</button>
+            <button className="btn btn-outline-secondary" onClick={onAdd(count)}>Add</button>
         </div>
 
     )
 }
 
-export default ItemCount
+export default ItemCount*/
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import ItemCount from '../ItemCount/ItemCount';
+import {useEffect, useState} from 'react'
+//import ItemCount from '../ItemCount/ItemCount';
+import customFetch from '../utils/customFetch'
+
+
+const dataFromBD = [];
 
 const ItemListContainer = () => {
-  const onAdd = (count) => {
+  const [data, setData] = useState ([]);
+
+  useEffect(() =>{
+    customFetch(2000, dataFromBD)
+      .then(datos => setData(dataFromBD));
+  }, []);
+  return(
+    <>
+    </>
+  );
+  
+
+
+
+
+
+ /* const onAdd = (count) => {
     alert("Seleccionaste " + count +" items");
   }
   return (
     <ItemCount stock ={10} initial={1} onAdd={onAdd}/>
-  );
+  );*/
 }
 
 export default ItemListContainer;
